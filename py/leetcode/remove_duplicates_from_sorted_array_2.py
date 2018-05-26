16ffcf4fc647fc523421ef16fe9b8e3a885da16d
Victor Alvarez <v.alvarez312@gmail.com>
2018-05-25 23:14:34 -0700

    removing duplicates from sorted array

---
+++ py/leetcode/remove_duplicates_from_sorted_array_2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeDuplicates(self, xs):
+        if not xs:
+            return 0
+        w = 1
+        for j in range(1, len(xs)):
+            if xs[j] != xs[j]:
+                xs[w] = xs[j]
+                w += 1
+        return w
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    xs = [1, 1, 2]
+    n = s.removeDuplicates(xs)
+    assert n == 2, n
+    assert xs[:n] == [1, 2], xs[:n]
+
+    xs = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    n = s.removeDuplicates(xs)
+    assert n == 5, n
+    assert xs[:n] == [0, 1, 2, 3, 4], xs[:n]
