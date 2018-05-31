e958a7a662148b0c51b5f1c4fb0f3cdd5f74b41c
Victor Alvarez <v.alvarez312@gmail.com>
2018-05-30 20:12:35 -0700

    mutate existing for optimal space

--- py/leetcode/minimum_path_sum.py
+++ py/leetcode/minimum_path_sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def minPathSum(self, grid):
-        '''Minimum path sum in T(m, n) = S(m, n) = O(m*n).'''
+        '''Minimum path sum in T(m, n) = O(m*n), S(m, n) = O(1),
+        mutates grid to achieve O(1) space complexity.
+        '''
         if not grid or not grid[0]:
             return 0
         m, n = len(grid), len(grid[0])
-        memo = [[None for _ in range(n)] for _ in range(m)]
-        memo[0][0] = grid[0][0]
         for i in range(1, m):
-            memo[i][0] = grid[i][0] + memo[i-1][0]
+            grid[i][0] = grid[i][0] + grid[i-1][0]
         for j in range(1, n):
-            memo[0][j] = grid[0][j] + memo[0][j-1]
+            grid[0][j] = grid[0][j] + grid[0][j-1]
         for j in range(1, n):
             for i in range(1, m):
-                memo[i][j] = min(memo[i-1][j], memo[i][j-1]) + grid[i][j]
-        return memo[m-1][n-1]
+                grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+        return grid[m-1][n-1]
