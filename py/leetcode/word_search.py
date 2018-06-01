0cfa66f0de47f728f52a1883d386321361341d74
Victor Alvarez <v.alvarez312@gmail.com>
2018-05-31 19:07:34 -0700

    use O(1) space

--- py/leetcode/word_search.py
+++ py/leetcode/word_search.py
@@ -1,35 +1,37 @@
+visiting = object()
+
+
 class Solution:
     def __init__(self):
         self.grid = None
         self.word = None
 
-    def _exist(self, i, j, pos, seen):
+    def _exist(self, i, j, pos):
         if self.grid[i][j] != self.word[pos]:
             return False
         if pos == len(self.word)-1:
             return True
-        seen.add((i, j))
         m, n = len(self.grid), len(self.grid[0])
+        t, self.grid[i][j] = self.grid[i][j], visiting
         for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
             if min(i+di, j+dj) < 0 or i+di >= m or j+dj >= n:
                 continue
-            elif (i+di, j+dj) in seen:
+            elif self.grid[i+di][j+dj] == visiting:
                 continue
-            if self._exist(i+di, j+dj, pos+1, seen):
+            if self._exist(i+di, j+dj, pos+1):
                 return True
-        seen.remove((i, j))
+        self.grid[i][j] = t
         return False
 
     def exist(self, grid, word):
-        '''Word search in T(m,n) = O(m*n*w) and S(n) = O(w).'''
+        '''Word search in T(m,n) = O(m*n*w) and S(n) = O(1).'''
         if not grid or not grid[0] or not word:
             return False
         self.grid = grid
         self.word = word
         m, n = len(self.grid), len(self.grid[0])
-        seen = set()
         for i in range(m):
             for j in range(n):
-                if self._exist(i, j, 0, seen):
+                if self._exist(i, j, 0):
                     return True
         return False
