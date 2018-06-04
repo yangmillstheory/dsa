520dc3b70fcd5eb05f76324b9c4f8c73cd7d32c6
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-03 21:12:02 -0700

    word squares

---
+++ py/other/word_squares.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+from io import StringIO
+
+
+class Trie:
+    def __init__(self):
+        self.links = defaultdict(self.__class__)
+        self.is_word = False
+
+    def insert(self, word):
+        trie = self
+        for ch in word:
+            trie = trie.links[ch]
+        trie.is_word = True
+
+    def search(self, prefix):
+        trie = self
+        for ch in prefix:
+            if ch not in trie.links:
+                return None
+            trie = trie.links[ch]
+        return trie
+
+    def get_children(self, prefix, children=None):
+        # note that this assumes that prefix is a prefix of self
+        if children is None:
+            children = []
+        trie = self
+        if trie.is_word:
+            children.append(prefix)
+        else:
+            for ch, node in trie.links.items():
+                node.get_children(prefix + ch, children)
+        return children
+
+
+def word_squares(words):
+    '''Find all valid word squares in O(n*k) time and O(n) space.'''
+    res = []
+    root = Trie()
+    for word in words:
+        root.insert(word)
+    for word in words:
+        _backtrack(root, len(word), [word], res)
+    return res
+
+
+def _backtrack(root, n, sq, res):
+    if len(sq) == n:
+        res.append(sq[:])
+    else:
+        j = len(sq)
+        buf = StringIO()
+        for i in range(j):
+            buf.write(sq[i][j])
+        prefix = buf.getvalue()
+        trie = root.search(prefix)
+        if trie:
+            words = trie.get_children(prefix)
+            for word in words:
+                _backtrack(root, n, sq + [word], res)
+    return res
+
+
+if __name__ == '__main__':
+    res = word_squares(['BALL', 'LEAD', 'AREA', 'LADY'])
+    assert res == [['BALL', 'AREA', 'LEAD', 'LADY']], res
