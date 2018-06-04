1ab9f4e5591524afd808cdcecbcc60711d8b950c
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-04 08:53:37 -0700

    permutations

---
+++ py/leetcode/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def swap(self, xs, i, j):
+        xs[i], xs[j] = xs[j], xs[i]
+
+    def permute(self, xs, i=0, res=None):
+        if res is None:
+            res = []
+        if xs is None:
+            return res
+        n = len(xs)
+        if i >= len(xs):
+            res.append(xs[:])
+        else:
+            for j in range(i, n):
+                self.swap(xs, i, j)
+                self.permute(xs, i+1, res)
+                self.swap(xs, i, j)
+        return res
