23fff0b3f6309531eaf424d7ba429b5c2c64f636
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-10 15:54:09 -0700

    add a better solution

--- py/leetcode/search_a_2d_matrix_ii.py
+++ py/leetcode/search_a_2d_matrix_ii.py
@@ -11,3 +11,19 @@ class Solution(object):
             if j < len(row) and row[j] == x:
                 return True
         return False
+
+    def searchMatrix2(self, g, x):
+        # note that this works for https://leetcode.com/problems/search-a-2d-matrix
+        # but is slower than two binary searches; here, it's faster than the above.
+        if not g or not g[0]:
+            return False
+        m, n = len(g), len(g[0])
+        i, j = 0, n-1
+        while i < m and j >= 0:
+            if g[i][j] == x:
+                return True
+            if g[i][j] < x:
+                i += 1
+            else:
+                j -= 1
+        return False
