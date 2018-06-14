351ab3b20304933b25fe03464435fdff5892d161
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-13 18:40:05 -0700

    move stuff; string processing problem

---
+++ py/leetcode/shortest_palindrome.py
@@ -0,0 +1,37 @@
+class Solution:
+    # T(n) = O(n^2)
+    def shortestPalindromeSlow(self, word):
+        n = len(word)
+        if not n:
+            return ''
+        pre = ''
+        for j in reversed(range(n)):
+            b = 0
+            for i in reversed(range(j+1)):
+                if i == b or word[i] != word[b]:
+                    break
+                b += 1
+            if i <= b:
+                break
+            if i > b:
+                pre += word[j]
+        return pre+word
+
+    # T(n) = O(n)
+    # S(n) = O(1)
+    def shortestPalindrome(self, word):
+        n = len(word)
+        if not n:
+            return ''
+        rev, i = word[::-1], 0
+        for j in range(1, n+1):
+            if rev[-j:] == word[:j]:
+                i = j
+        return rev[:n-i]+word
+
+
+if __name__ == '__main__':
+    s = Solution()
+    assert s.shortestPalindrome("abcd") == 'dcbabcd'
+    assert s.shortestPalindrome("aacecaaa") == 'aaacecaaa'
+    assert s.shortestPalindrome("abbacd") == 'dcabbacd'
