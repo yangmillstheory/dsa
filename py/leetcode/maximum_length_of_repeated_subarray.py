e639206e8588fac67b8963e086aaaf57e81cf92a
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-15 01:16:34 -0700

    a dp problem

---
+++ py/leetcode/maximum_length_of_repeated_subarray.py
@@ -0,0 +1,40 @@
+class Solution:
+    def find_length_brute_force(self, a, b):
+        # O(n^4) solution
+        n = len(a)
+        max_len = 0
+        for i in range(n):
+            for j in range(i+1, n+1):
+                sub_a = a[i:j]
+                for k in range(n-j+i+1):
+                    if sub_a == b[k:k+j-i]:
+                        max_len = max(max_len, j-i)
+        return max_len
+
+    def find_length_dp_1(self, a, b):
+        # T(n) = O(n^2)
+        # S(n) = O(n^2)
+        n = len(a)
+        max_len = 0
+        # memo[i][j] is the solution for the subproblem on a[:i] and b[:j]
+        memo = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if a[i-1] == b[i-1]:
+                    memo[i][j] = memo[i-1][j-1] + 1
+                    max_len = max(max_len, memo[i][j])
+        return max_len
+
+    def findLength(self, a, b):
+        # T(n) = O(n^2)
+        # S(n) = O(n)
+        n = len(a)
+        max_len = 0
+        # memo[i][j] is the solution for the subproblem on a[:i] and b[:j]
+        memo = [0 for _ in range(n+1)]
+        for i in range(1, n+1):
+            for j in range(1, n+1, -1):
+                if a[i-1] == b[j-1]:
+                    memo[j] = memo[j-1] + 1
+                    max_len = max(max_len, memo[j])
+        return max_len
