0e539b149f94e5d1111348ba4683241205b90904
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-18 21:59:59 -0700

    more clever solution

--- py/leetcode/longest_increasing_subsequence.py
+++ py/leetcode/longest_increasing_subsequence.py
@@ -1,12 +1,28 @@
+import bisect
+
+
 class Solution(object):
-    def lengthOfLIS(self, a):
+    def length_of_lis_dp(self, xs):
         # T(n) = O(n^2)
         # S(n) = O(n)
-        if not a:
+        if not xs:
             return 0
-        n = len(a)
-        # dp[i] is the length of a max LIS ending at a[i]
+        n = len(xs)
+        # dp[i] is the length of a max LIS ending at xs[i]
         dp = [1]*n
         for i in range(n):
-            dp[i] = max([dp[j]+1 for j in range(i) if a[j] < a[i]] + [1])
+            dp[i] = max([dp[j]+1 for j in range(i) if xs[j] < xs[i]] + [1])
         return max(dp)
+
+    def lengthOfLIS(self, xs):
+        # T(n) = O(n*log n)
+        # S(n) = O(n)
+        # see http://qr.ae/TUp4ph
+        lis = []
+        for x in xs:
+            i = bisect.bisect_left(lis, x)
+            if i == len(lis):
+                lis.append(x)
+            else:
+                lis[i] = x
+        return len(lis)
