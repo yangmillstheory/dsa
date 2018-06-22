658664fffcc18fd96cb131c2b382e8886ad2babd
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-21 20:14:06 -0700

    more pruning

--- py/leetcode/word_search.py
+++ py/leetcode/word_search.py
@@ -8,14 +8,15 @@ class Solution:
         self.seen = None
 
     def _exist(self, i, j, pos):
-        entry = self.grid[i][j]
-        if (i, j, pos) in self.bad or entry != self.word[pos]:
+        if (i, j, pos) in self.bad:
+            return False
+        if self.grid[i][j] != self.word[pos]:
             self.bad.add((i, j, pos))
             return False
         if pos == len(self.word)-1:
             return True
         m, n = len(self.grid), len(self.grid[0])
-        t, self.grid[i][j] = entry, VISITING
+        t, self.grid[i][j] = self.grid[i][j], VISITING
         for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
             if min(i+di, j+dj) < 0 or i+di >= m or j+dj >= n:
                 continue
