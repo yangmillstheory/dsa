2d5e7c49c3b05ab533acf2a088a82a0372002903
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-28 19:15:50 -0700

    clone a graph

---
+++ py/leetcode/clone_graph.py
@@ -0,0 +1,21 @@
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+
+class Solution:
+    def cloneGraph(self, v):
+        clones = {}
+
+        def _clone(t):
+            if t is None:
+                return None
+            if t in clones:
+                return clones[t]
+            clone = clones.setdefault(t, UndirectedGraphNode(t.label))
+            for u in t.neighbors:
+                clone.neighbors.append(clones.get(u) or _clone(u))
+            return clone
+        return _clone(v)
