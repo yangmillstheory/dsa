56379f5d16258dfc847c128d1c35ef24c19ca134
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-28 21:57:05 -0700

    some optimization

--- py/leetcode/word_ladder.py
+++ py/leetcode/word_ladder.py
@@ -5,7 +5,14 @@ class Solution(object):
     def ladderLength(self, s, t, words):
         words.append(s)
         words = set(words)
-        alpha = {ch for word in words for ch in word}
+
+        def get_alpha():
+            alpha = collections.defaultdict(set)
+            for word in words:
+                for i, ch in enumerate(word):
+                    alpha[i].add(ch)
+            return alpha
+        alpha = get_alpha()
         q, seen = collections.deque([(s, 1)]), set()
         while q:
             v, n = q.popleft()
@@ -13,7 +20,7 @@ class Solution(object):
                 return n
             chars = list(v)
             for i, ch in enumerate(v):
-                for _ch in alpha:  # only use characters for edges that come from words
+                for _ch in alpha[i]:  # only use characters for edges that come from words
                     chars[i] = _ch
                     u = ''.join(chars)
                     if u in words and u not in seen and u != v:
