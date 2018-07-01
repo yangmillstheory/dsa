3e5f115b274ac0f157d55276f0711948fb7ae00c
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-01 00:04:13 -0700

    cleanup

--- py/leetcode/implement_trie_prefix_tree.py
+++ py/leetcode/implement_trie_prefix_tree.py
@@ -43,10 +43,3 @@ class Trie:
                 return False
             node = node.children[ch]
         return True
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
