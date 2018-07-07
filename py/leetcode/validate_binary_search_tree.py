b9f0f9ce08482da8f4888fa30f3dcf27219a98c8
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-06 18:39:56 -0700

    adding more solutions

--- py/leetcode/validate_binary_search_tree.py
+++ py/leetcode/validate_binary_search_tree.py
@@ -1,9 +1,44 @@
+from collections import deque
+
+
 class Solution(object):
-    def isValidBST(self, root, key_min=float('-inf'), key_max=float('inf')):
+    def _preorder(self, tree, key_min=float('-inf'), key_max=float('inf')):
         # T(n) = S(n) = O(n)
-        if not root:
+        if not tree:
             return True
-        l, r, val = root.left, root.right, root.val
+        l, r, val = tree.left, tree.right, tree.val
         if val <= key_min or val >= key_max:
             return False
-        return self.isValidBST(l, key_min, min(key_max, val)) and self.isValidBST(r, max(key_min, val), key_max)
+        return self._preorder(l, key_min, min(key_max, val)) and self._preorder(r, max(key_min, val), key_max)
+
+    def _inorder(self, tree):
+        stack, prev = [], float('-inf')
+        while stack or tree:
+            if tree:
+                stack.append(tree)
+                tree = tree.left
+            else:
+                tree = stack.pop()
+                if tree.val <= prev:
+                    return False
+                prev = tree.val
+                tree = tree.right
+        return True
+
+    def _bfs(self, tree):
+        if not tree:
+            return True
+        q = deque([(tree, float('-inf'), float('inf'))])
+        while q:
+            tree, key_min, key_max = q.popleft()
+            l, r, val = tree.left, tree.right, tree.val
+            if val <= key_min or val >= key_max:
+                return False
+            if l:
+                q.append((l, key_min, min(key_max, val)))
+            if r:
+                q.append((r, max(key_min, val), key_max))
+        return True
+
+    def isValidBST(self, root):
+        return self._bfs(root)
