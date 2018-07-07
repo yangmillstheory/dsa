b7b49000097d2f95a9e1c6900dc7a5b94f695288
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-07 14:38:19 -0700

    adding shift operator

--- py/leetcode/next_greater_element_ii.py
+++ py/leetcode/next_greater_element_ii.py
@@ -1,12 +1,33 @@
 class Solution(object):
-    def nextGreaterElements(self, a):
-        # T(n) = S(n) = O(n)
+    def _with_shift(self, a):
+        # T(n) = O(n) (2 iterations)
+        # S(n) = O(n) (list of size n)
+        n = len(a)
+        res = [-1]*n
+        prev_max_right = []
+        for j in range(2*n-2, -1, -1):
+            i = min(j-n, j)
+            if a[i] < a[i+1]:
+                res[i] = a[i+1]
+                prev_max_right.append(a[i+1])
+            else:
+                max_right = -1
+                while prev_max_right and prev_max_right[-1] <= a[i]:
+                    prev_max_right.pop()
+                if prev_max_right:
+                    max_right = prev_max_right[-1]
+                else:
+                    prev_max_right.append(a[i])
+                res[i] = max_right
+        return res
+
+    def _without_shift(self, a):
+        # T(n) = O(n) (2*n iterations)
+        # S(n) = O(n) (list of size 2*n)
         m = len(a)
         n = 2*m
         a += a
         res = [-1]*n
-        if not a:
-            return res
         prev_max_right = []
         for j in range(n-2, -1, -1):
             if a[j] < a[j+1]:
@@ -22,3 +43,6 @@ class Solution(object):
                     prev_max_right.append(a[j])
                 res[j] = max_right
         return res[:m]
+
+    def nextGreaterElements(self, a):
+        return self._with_shift(a)
