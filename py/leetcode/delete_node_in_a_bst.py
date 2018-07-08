ee2b26e162c71daeaa7fb02dee32645c3a159187
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 11:10:06 -0700

    cleanup

--- py/leetcode/delete_node_in_a_bst.py
+++ py/leetcode/delete_node_in_a_bst.py
@@ -8,9 +8,8 @@ class Solution(object):
             prev.left = tree.right
         return tree
 
-    def deleteNode(self, tree, key):
-        '''Deletes a node from a BST in O(h) time and O(1) space. The key doesn't have to exist in the tree.'''
-        prev, root = None, tree
+    def _find_with_prev(self, tree, key):
+        prev = None
         while tree:
             if tree.val == key:
                 break
@@ -18,12 +17,20 @@ class Solution(object):
                 prev, tree = tree, tree.right
             else:
                 prev, tree = tree, tree.left
+        return tree, prev
+
+    def deleteNode(self, tree, key):
+        '''Deletes a node from a BST in O(h) time and O(1) space. The key doesn't have to exist in the tree.'''
+        root = tree
+        tree, prev = self._find_with_prev(tree, key)
         if not tree:
             return root
         if tree.right:
             repl = self._delete_and_return_min(tree.right)
             repl.left = tree.left
             if repl != tree.right:
+                # special case: the minimum of any tree could be its root;
+                # we avoid that case since otherwise we'd create a cycle
                 repl.right = tree.right
         else:
             repl = tree.left
