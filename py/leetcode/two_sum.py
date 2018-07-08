40976dff252089dfc22d9a4a2bf79a2208ad55af
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 13:17:34 -0700

    more two-sum crap

--- py/leetcode/two_sum.py
+++ py/leetcode/two_sum.py
@@ -1,15 +1,7 @@
-import bisect
-
-
 class Solution(object):
     def twoSum(self, a, k):
         for i, x in enumerate(a):
             for j in range(i+1, len(a)):
                 if x+a[j] == k:
                     return [i, j]
-        raise ValueError
-
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.twoSum([3, 2, 4], 6))
+        raise ValueError('I was promised an input with a positive result!')
---
+++ py/leetcode/two_sum_ii_input_array_is_sorted.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, a, k):
+        lo, hi = 0, len(a)-1
+        while lo < hi:
+            cand = a[lo]+a[hi]
+            if cand < k:
+                lo += 1
+            elif cand > k:
+                hi -= 1
+            else:
+                return [lo+1, hi+1]
+        raise ValueError('I was promised an input with a positive result!')
