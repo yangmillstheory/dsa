6d80688b8fcdbef122ca1a30b4b106142e11436c
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 13:24:22 -0700

    two-sum problem with a BST

---
+++ py/leetcode/two_sum_iv_input_is_a_bst.py
@@ -0,0 +1,40 @@
+def _rev_order(tree):
+    stack = []
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.right
+        else:
+            tree = stack.pop()
+            yield tree
+            tree = tree.left
+
+
+def _in_order(tree):
+    stack = []
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.left
+        else:
+            tree = stack.pop()
+            yield tree
+            tree = tree.right
+
+
+class Solution(object):
+    def findTarget(self, tree, k):
+        '''Answer the Two-Sum query in O(h) time.'''
+        if not tree:
+            return False
+        fwd, rev = _in_order(tree), _rev_order(tree)
+        lo, hi = next(fwd), next(rev)
+        while lo != hi:
+            cand = lo.val + hi.val
+            if cand < k:
+                lo = next(fwd)
+            elif cand > k:
+                hi = next(rev)
+            else:
+                return True
+        return False
