cbe2fd1fd58bd9bed0359c665c95e1afd994a797
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 15:29:03 -0700

    build an index first

--- py/leetcode/minimum_absolute_difference_in_bst.py
+++ py/leetcode/minimum_absolute_difference_in_bst.py
@@ -10,19 +10,39 @@ def tree_max(tree):
     return tree
 
 
+def min_max_index(tree, min_ind=None, max_ind=None):
+    if not tree:
+        return min_ind, max_ind
+    if min_ind is None:
+        min_ind = {None: float('inf')}
+    if max_ind is None:
+        max_ind = {None: float('-inf')}
+    min_max_index(tree.left, min_ind, max_ind)
+    min_max_index(tree.right, min_ind, max_ind)
+    min_ind[tree] = min(min_ind[tree.left], min_ind[tree.right], tree.val)
+    max_ind[tree] = max(max_ind[tree.left], max_ind[tree.right], tree.val)
+    return min_ind, max_ind
+
+
 class Solution(object):
-    def getMinimumDifference(self, tree):
+    def _recursive(self, tree, min_ind, max_ind):
         '''Get the minimum absolute difference between any two nodes in a BST in
-        T(n) = O(n*2^n) time and O(h) space.
+        T(n) = O(n) time and O(h) space.
+
+        This relies on a prebuilt index of subtree minima and maxima, indexed by node;
+        in tests, this is actually slower than calling tree_min and tree_max per node.
         '''
         if not tree:
             return float('inf')
         l, r, val = tree.left, tree.right, tree.val
         cands = []
         if l:
-            cands.append(abs(val-tree_max(l).val))
+            cands.append(abs(val-max_ind[l]))
         if r:
-            cands.append(abs(val-tree_min(r).val))
-        cands.append(self.getMinimumDifference(l))
-        cands.append(self.getMinimumDifference(r))
+            cands.append(abs(val-min_ind[r]))
+        cands.append(self._recursive(l))
+        cands.append(self._recursive(r))
         return min(*cands)
+
+    def getMinimumDifference(self, tree):
+        return self._recursive(tree, *min_max_index(tree))
