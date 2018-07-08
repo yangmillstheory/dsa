f733390496db68be618ceee7a538608c19ef50f9
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 16:02:21 -0700

    BST to greater tree

---
+++ py/leetcode/convert_bst_to_greater_tree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convertBST(self, root):
+        '''Convert a BST to a Greater Tree in O(n) time and O(h) space.'''
+        def _rev_order(tree):
+            stack = []
+            while stack or tree:
+                if tree:
+                    stack.append(tree)
+                    tree = tree.right
+                else:
+                    tree = stack.pop()
+                    yield tree
+                    tree = tree.left
+        prev = 0
+        for tree in _rev_order(root):
+            tree.val += prev
+            prev = tree.val
