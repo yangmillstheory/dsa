cefbf25d389da419e126903ced298788fa1ba691
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 23:46:40 -0700

    don't mutate

--- py/leetcode/string_compression.py
+++ py/leetcode/string_compression.py
@@ -2,7 +2,24 @@ TERM = '-'
 
 
 class Solution(object):
-    def compress(self, chars):
+    def _compress_wo_sentinel(self, chars):
+        '''Run-length compression on a list of characters in O(n) time and O(1) space.'''
+        n = len(chars)
+        i, count = 0, 1
+        for j in range(1, n+1):
+            if j < n and chars[j] == chars[j-1]:
+                count += 1
+            else:
+                chars[i] = chars[j-1]
+                i += 1
+                if count > 1:
+                    for d in str(count):
+                        chars[i] = d
+                        i += 1
+                count = 1
+        return i
+
+    def _compress_w_sentinel(self, chars):
         '''Run-length compression on a list of characters in O(n) time and O(1) space.'''
         i = count = 0
         chars.append(TERM)
@@ -19,3 +36,7 @@ class Solution(object):
                 count = 1
         chars.pop()
         return i
+
+    def compress(self, chars):
+        return self._compress_wo_sentinel(chars)
+
