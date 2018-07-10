ad9420b13eba75c277be9b7cfe42f3e1ee8b87aa
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-09 23:20:16 -0700

    fuck this problem

--- py/leetcode/reverse_words_in_a_string.py
+++ py/leetcode/reverse_words_in_a_string.py
@@ -1,27 +1,24 @@
-DELIMITER = ' '
-
-
-def reverse(seq, lo, hi):
-    while lo <= hi:
-        seq[lo], seq[hi] = seq[hi], seq[lo]
-        lo += 1
-        hi -= 1
+import re
 
 
 class Solution(object):
     def reverseWords(self, s):
         # sanitize
-        s = s.strip()
-        chars = []
-        for i, ch in enumerate(s):
-            if ch != DELIMITER or (i and s[i-1] != DELIMITER):
-                chars.append(ch)
+        s = re.sub(r'\s{2,}', ' ', s.strip())
         # reverse entire string, then reverse each word
-        n = len(chars)
-        reverse(chars, 0, n-1)
-        i = 0
+        s = s[::-1]
+        res, i, n = [], 0, len(s)
         for j in range(n+1):
-            if j == n or chars[j] == DELIMITER:
-                reverse(chars, i, j-1)
-                i = j+1
-        return ''.join(chars)
+            if j == n or s[j].isspace():
+                if not i:
+                    t = s[j-1::-1]
+                else:
+                    t = s[j-1:i:-1]
+                res.append(t)
+                i = j
+        return ' '.join(res)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseWords('the sky is blue'))
