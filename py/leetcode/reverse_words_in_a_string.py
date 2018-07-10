e905d42c0b12969aafbd504070f5cedecbf9e61e
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-09 22:45:37 -0700

    comment

--- py/leetcode/reverse_words_in_a_string.py
+++ py/leetcode/reverse_words_in_a_string.py
@@ -10,11 +10,13 @@ def reverse(seq, lo, hi):
 
 class Solution(object):
     def reverseWords(self, s):
+        # do some cleanup
         s = s.strip()
         chars = []
         for i, ch in enumerate(s):
             if ch != DELIMITER or (i and s[i-1] != DELIMITER):
                 chars.append(ch)
+        # reverse entire string, then reverse each word
         n = len(chars)
         reverse(chars, 0, n-1)
         i = 0
