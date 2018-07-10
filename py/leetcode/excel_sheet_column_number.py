ee72f43886711f7c9d2f0656f3f174057e84565d
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-09 19:10:03 -0700

    a mathy prob

---
+++ py/leetcode/excel_sheet_column_number.py
@@ -0,0 +1,11 @@
+n_chars = ord('Z')-ord('A')+1
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        '''Return the Excel spreadsheet column ID in O(n) time.'''
+        col_id = 0
+        for i, ch in enumerate(s):
+            c = ord(ch)-ord('A')+1
+            col_id += c*pow(n_chars, i)
+        return col_id
