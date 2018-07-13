6a6e940b7b9da537ec44ef85d778a5c73f7c2fcf
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-12 19:09:17 -0700

    simple array processing prob.

--- py/leetcode/remove_duplicates_from_sorted_array.py
+++ py/leetcode/remove_duplicates_from_sorted_array.py
@@ -1,24 +1,14 @@
-class Solution:
-    def removeDuplicates(self, xs):
-        if not xs:
-            return 0
-        last, w = xs[0], 1
-        for i in range(1, len(xs)):
-            if xs[i] > last:
-                last = xs[w] = xs[i]
-                w += 1
+class Solution(object):
+    def removeDuplicates(self, a):
+        '''Remove duplicates from a sorted array in place in O(n) time.'''
+        i = w = 0
+        n = len(a)
+        while i < n:
+            while i and i < n and a[i] == a[i-1]:
+                i += 1
+            if i == n:
+                break
+            a[w] = a[i]
+            i += 1
+            w += 1
         return w
-
-
-if __name__ == '__main__':
-    s = Solution()
-
-    xs = [1, 1, 2]
-    n = s.removeDuplicates(xs)
-    assert n == 2, n
-    assert xs[:n] == [1, 2], xs[:n]
-
-    xs = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-    n = s.removeDuplicates(xs)
-    assert n == 5, n
-    assert xs[:n] == [0, 1, 2, 3, 4], xs[:n]
