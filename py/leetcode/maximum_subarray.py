ba6591d7ae0267df7eceb974229121f03095dd3f
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-14 17:07:07 -0700

    clean

--- py/leetcode/maximum_subarray.py
+++ py/leetcode/maximum_subarray.py
@@ -1,7 +1,5 @@
 class Solution(object):
-    def maxSubArray(self, xs, thing=None):
-        # prev is the maximum sum ending at x of "previous iteration";
-        # we can extend it or start a new maximum sum beginning at x
+    def maxSubArray(self, xs):
         prev, max_seen = 0, float('-inf')
         for x in xs:
             prev = max(prev+x, x)
