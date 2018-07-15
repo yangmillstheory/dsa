84c9e5ff305b1eb99af2fbc4406449214cc3c4c7
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-14 19:01:00 -0700

    random shuffle

---
+++ py/leetcode/shuffle_an_array.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+class Solution(object):
+    def __init__(self, a):
+        self._orig = [x for x in a]
+        self._a = a
+
+    def reset(self):
+        self._a = [x for x in self._orig]
+        return self._a
+
+    def shuffle(self):
+        n, a = len(self._a), self._a
+        i = 0
+        for _ in range(n):
+            j = randint(i, n-1)
+            a[i], a[j] = a[j], a[i]
+            i += 1
+        return a
