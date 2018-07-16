35fb6278ed0633ac15229733dbe10c83f14ccf51
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-15 21:00:41 -0700

    clean

--- py/leetcode/reverse_linked_list_ii.py
+++ py/leetcode/reverse_linked_list_ii.py
@@ -8,13 +8,10 @@ def get_kth(head, k):
 class Solution(object):
     def reverseBetween(self, head, s, f):
         '''Reverse a sublist of a singly-linked list in O(f) time.'''
-        if not head:
-            return
-        if s == 1:
-            prev_s, s_node = ListNode(0), head
-        else:
-            prev_s = get_kth(head, s-1)
-            s_node = prev_s.next
+        dummy = prev_s = ListNode(0)
+        dummy.next = head
+        prev_s = get_kth(dummy, s)
+        s_node = prev_s.next
         f_node = get_kth(s_node, f-s+1)
         while s_node != f_node:
             next_s_node, s_node.next = s_node.next, f_node.next
@@ -22,4 +19,4 @@ class Solution(object):
             s_node = s_node.next
             s_node = next_s_node
         prev_s.next = f_node
-        return prev_s.next if s == 1 else head
+        return dummy.next
