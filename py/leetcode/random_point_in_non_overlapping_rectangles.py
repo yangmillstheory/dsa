5c7af206b47da35d58a069d8d873b2b636ddee6a
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-29 15:17:39 -0700

    prob.

---
+++ py/leetcode/random_point_in_non_overlapping_rectangles.py
@@ -0,0 +1,24 @@
+import bisect
+import random
+
+
+def area(x1, y1, x2, y2):
+    return (x2-x1+1)*(y2-y1+1)
+
+
+class Solution(object):
+    def __init__(self, rects):
+        self._rects = rects
+        self._areas, s = [], 0
+        for rect in self._rects:
+            s += area(*rect)
+            self._areas.append(s)
+
+    def pick(self):
+        x = random.randint(1, self._areas[-1])
+        j = bisect.bisect_left(self._areas, x)
+        x1, y1, x2, y2 = self._rects[j]
+        return [
+            random.randint(x1, x2),
+            random.randint(y1, y2),
+        ]
