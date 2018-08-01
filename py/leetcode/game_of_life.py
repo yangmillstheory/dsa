90d711bf174981538518dcd4615bde534e0171c4
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-01 07:33:30 -0700

    simpler

--- py/leetcode/game_of_life.py
+++ py/leetcode/game_of_life.py
@@ -1,42 +1,10 @@
-_prev_0_curr_0 = 0, 0
-_prev_0_curr_1 = 0, 1
-_prev_1_curr_0 = 1, 0
-_prev_1_curr_1 = 1, 1
-
-
-def read(g, i, j):
-    if isinstance(g[i][j], tuple):
-        return g[i][j]
-    return g[i][j], g[i][j]
-
-
-def write(g, i, j, curr):
-    prev = read(g, i, j)[0]
-    if not prev and not curr:
-        g[i][j] = _prev_0_curr_0
-    elif prev and not curr:
-        g[i][j] = _prev_1_curr_0
-    elif curr and not prev:
-        g[i][j] = _prev_0_curr_1
-    else:
-        g[i][j] = _prev_1_curr_1
-
-
 def n_adj(g, i, j):
     m, n = len(g), len(g[0])
     s = 0
-    for di, dj in [
-        [-1, +1],
-        [-1, -1],
-        [-1, 0],
-        [0, +1],
-        [0, -1],
-        [+1, +1],
-        [+1, -1],
-        [+1, 0],
-    ]:
-        if min(i+di, j+dj) >= 0 and i+di < m and j+dj < n and read(g, i+di, j+dj)[0]:
-            s += 1
+    for di in range(-1, 2):
+        for dj in range(-1, 2):
+            if (di or dj) and min(i+di, j+dj) >= 0 and i+di < m and j+dj < n and g[i+di][j+dj] in {1, 3}:
+                s += 1
     return s
 
 
@@ -50,22 +18,13 @@ class Solution(object):
         for i in range(m):
             for j in range(n):
                 s = n_adj(g, i, j)
-                if read(g, i, j)[0]:
-                    if s < 2 or s > 3:
-                        write(g, i, j, 0)
+                if g[i][j] == 1 and 1 < n < 4:
+                    g[i][j] += 2
                 elif s == 3:
-                    write(g, i, j, 1)
+                    g[i][j] += 2
         for i in range(m):
             for j in range(n):
-                g[i][j] = read(g, i, j)[1]
+                g[i][j] = 1 if g[i][j] > 1 else 0
 
     def gameOfLife(self, g):
         return self._game_of_life_optimized(g)
-
-
-if __name__ == '__main__':
-    s = Solution()
-    g = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
-    s.gameOfLife(g)
-    import pprint
-    pprint.pprint(g)
