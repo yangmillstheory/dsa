3e94a7489819180d5a3f963a6e8a5b276bbefee9
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-31 20:03:23 -0700

    clean

--- py/leetcode/island_perimeter.py
+++ py/leetcode/island_perimeter.py
@@ -1,40 +1,34 @@
-_visiting = object()
-
-
-def perimeter(g):
-    '''Compute the perimeter of an island in O(m*n) time and space.'''
-    if not g or not g[0]:
-        return 0
-    m, n = len(g), len(g[0])
-    p = {'value': 0}
-
-    def adj(i, j):
-        return (
-            (i+di, j+dj)
-            for di, dj in (
-                (di, dj)
-                for di in range(-1, 2)
-                for dj in range(-1, 2)
-                if abs(di) != abs(dj) and
-                i+di < m and j+dj < n and min(i+di, j+dj) >= 0
-            ))
-
-    def dfs(i, j):
-        p['value'] += 4
-        g[i][j] = _visiting
-        for x, y in adj(i, j):
-            if g[x][y] == 1:
-                p['value'] -= 1
-                dfs(x, y)
-            elif g[x][y] == _visiting:
-                p['value'] -= 1
-    for i in range(m):
-        for j in range(n):
-            if g[i][j] == 1:
-                dfs(i, j)
-    return p['value']
-
-
 class Solution(object):
+    def __init__(self):
+        self.n = 0
+
     def islandPerimeter(self, g):
-        return perimeter(g)
+        if not g or not g[0]:
+            return 0
+        m, n = len(g), len(g[0])
+
+        def adj(i, j):
+            return [
+                (i+di, j+dj)
+                for di, dj in ((di, dj)
+                    for di in range(-1, 2)
+                    for dj in range(-1, 2)
+                    if abs(di) != abs(dj) and i+di < m and j+dj < n and min(i+di, j+dj) >= 0)
+            ]
+
+        _visiting = 'X'
+
+        def dfs(i, j):
+            self.n += 4
+            g[i][j] = _visiting
+            for x, y in adj(i, j):
+                if g[x][y] == 1:
+                    self.n -= 1
+                    dfs(x, y)
+                elif g[x][y] == _visiting:
+                    self.n -= 1
+        for i in range(m):
+            for j in range(n):
+                if g[i][j] == 1:
+                    dfs(i, j)
+        return self.n
