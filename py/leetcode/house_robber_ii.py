d4a98a7a70d2b822dedaeb1f8758e8dbd7db02d0
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-04 15:31:37 -0700

    reclaim some dignity

--- py/leetcode/house_robber_ii.py
+++ py/leetcode/house_robber_ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def rob(self, houses):
-        def rob_straight(lo, hi):
-            a, b = 0, 0
-            for i in range(lo, hi+1):
-                a, b = b, max(houses[i]+a, b)
-            return b
         n = len(houses)
         if n == 0:
             return 0
         if n <= 2:
             return max(houses)
-        return max(rob_straight(0, n-2), rob_straight(1, n-1))
+        a = b = c = d = 0
+        best = 0
+        for i, x in enumerate(houses):
+            if i < n-1:
+                a, b = b, max(houses[i]+a, b)
+            if i > 0:
+                c, d = d, max(houses[i]+c, d)
+            best = max(best, b, d)
+        return best
