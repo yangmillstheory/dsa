107a0ef3c435b8572cb76d6ef7091279afcf7417
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-04 22:34:02 -0700

    a problem

---
+++ py/leetcode/find_right_interval.py
@@ -0,0 +1,27 @@
+import operator
+
+
+def first_larger(intervals, lo, hi, q):
+    lowest = -1
+    while lo <= hi:
+        mid = lo+(hi-lo)//2
+        if intervals[mid].start >= q:
+            lowest = mid
+            hi = mid-1
+        else:
+            lo = mid+1
+    return lowest
+
+
+class Solution(object):
+    def findRightInterval(self, intervals):
+        # T(n) = O(n*log(n))
+        # S(n) = O(n)
+        n, ind = len(intervals), {interval: i for i, interval in enumerate(intervals)}
+        intervals.sort(key=operator.attrgetter('start'))
+        res = [-1]*n
+        for i, interval in enumerate(intervals):
+            j = first_larger(intervals, i+1, n-1, interval.end)
+            if j != -1:
+                res[ind[interval]] = ind[intervals[j]]
+        return res
