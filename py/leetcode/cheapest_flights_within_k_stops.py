115eecbac5e2b011d9723c129ddc7e058bad45c6
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-11 22:35:04 -0700

    graph prob.

--- py/leetcode/cheapest_flights_within_k_stops.py
+++ py/leetcode/cheapest_flights_within_k_stops.py
@@ -5,8 +5,10 @@ class Solution(object):
     def __init__(self):
         self._min_cost = float('inf')
 
-    def findCheapestPrice(self, n, flights, src, dst, max_stops):
-        '''Find the cheapest cost from source City to destination City in O(n) time and space.'''
+    def findCheapestFlight(self, *args):
+        return self._bfs(*args)
+
+    def _dfs(self, n, flights, src, dst, max_stops):
         flights_by_city = collections.defaultdict(list)
         for _src, _dst, cost in flights:
             flights_by_city[_src].append((_dst, cost))
@@ -22,4 +24,20 @@ class Solution(object):
                         continue
                     dfs(_city, cost_to_dest, stops)
         dfs(src, 0, 0)
+        return self._min_cost if self._min_cost != float('inf') else -1
+
+    def _bfs(self, n, flights, src, dst, max_stops):
+        '''Find the cheapest cost from source City to destination City in O(n) time and space.'''
+        flights_by_city = collections.defaultdict(list)
+        for _src, _dst, cost in flights:
+            flights_by_city[_src].append((_dst, cost))
+        q = collections.deque([(src, 0, 0)])
+        while q:
+            city, cost, stops = q.pop()
+            stops += 1
+            for _city, _cost in flights_by_city[city]:
+                next_cost = cost + _cost
+                if next_cost >= self._min_cost or stops > max_stops:
+                    continue
+                q.append((_city, next_cost, stops))
         return self._min_cost if self._min_cost != float('inf') else -1
