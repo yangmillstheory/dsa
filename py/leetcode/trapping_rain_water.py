bc31f66e2f7af9e988be4b19c3b1f0f67ef5f3d0
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-11 22:10:06 -0700

    better solution

--- py/leetcode/trapping_rain_water.py
+++ py/leetcode/trapping_rain_water.py
@@ -1,23 +1,41 @@
 class Solution(object):
-    def trap(self, hs):
+    def _trap_ugly(self, a):
         # T(n) = S(n) = O(n)
-        if not hs:
+        if not a:
             return 0
-        n, res = len(hs), 0
-        l_heights = [hs[0]] + [float('-inf') for _ in range(n-1)]
-        r_heights = [float('-inf') for _ in range(n-1)] + [hs[-1]]
+        n, res = len(a), 0
+        l_heights = [a[0]] + [float('-inf') for _ in range(n-1)]
+        r_heights = [float('-inf') for _ in range(n-1)] + [a[-1]]
         for i in range(1, n):
-            if hs[i] < l_heights[i-1]:
+            if a[i] < l_heights[i-1]:
                 l_heights[i] = l_heights[i-1]
-            elif hs[i] < hs[i-1]:
-                l_heights[i] = hs[i-1]
+            elif a[i] < a[i-1]:
+                l_heights[i] = a[i-1]
         for i in range(n-2, -1, -1):
-            if hs[i] < r_heights[i+1]:
+            if a[i] < r_heights[i+1]:
                 r_heights[i] = r_heights[i+1]
-            elif hs[i] < hs[i+1]:
-                r_heights[i] = hs[i+1]
+            elif a[i] < a[i+1]:
+                r_heights[i] = a[i+1]
         for i in range(1, n-1):
             l_height, r_height = l_heights[i], r_heights[i]
             if r_height != float('-inf') and l_height != float('-inf'):
-                res += min(r_height, l_height)-hs[i]
+                res += min(r_height, l_height)-a[i]
         return res
+
+    def trap(self, a):
+        i, j = 0, len(a)-1
+        max_l, max_r, area = float('-inf'), float('-inf'), 0
+        while i < j:
+            if a[i] < a[j]:
+                if a[i] < max_l:
+                    area += max_l-a[i]
+                else:
+                    max_l = a[i]
+                i += 1
+            else:
+                if a[j] < max_r:
+                    area += max_r-a[j]
+                else:
+                    max_r = a[j]
+                j -= 1
+        return area
