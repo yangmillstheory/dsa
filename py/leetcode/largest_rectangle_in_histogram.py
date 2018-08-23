fa06a29373c962d385408272327dd5dc7069d9c7
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-22 22:15:08 -0700

    fucking hard problem

--- py/leetcode/largest_rectangle_in_histogram.py
+++ py/leetcode/largest_rectangle_in_histogram.py
@@ -1,15 +1,10 @@
-from collections import namedtuple
-
-Pillar = namedtuple('Pillar', ['i', 'h'])
-
-
 class Solution(object):
-    def largestRectangleArea(self, hs):
-        pillars, res = [], 0
-        for i, h in enumerate(hs + [0]):
-            while pillars and pillars[-1].h > h:
-                _i, _h = pillars.pop()
-                w = i - pillars[-1].i - 1 if pillars else i
-                res = max(res, w*_h)
-            pillars.append(Pillar(i, h))
-        return res
+    def largestRectangleArea(self, a):
+        stack, best = [], 0
+        for i, h in enumerate(a + [0]):
+            while stack and a[stack[-1]] > h:
+                j = stack.pop()
+                w = i-stack[-1]-1 if stack else i
+                best = max(best, w*a[j])
+            stack.append(i)
+        return best
