d745fa8dd8bc81b14805138ed87b2cf4699461cc
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-23 08:01:35 -0700

    clean

--- py/leetcode/longest_palindromic_substring.py
+++ py/leetcode/longest_palindromic_substring.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    def longestPalindrome(self, s):
-        best, n = '', len(s)
+    def longestPalindrome(self, string):
+        best, n = '', len(string)
+        s = f = 0
         for i in range(n):
             k = 0
-            while i+k < n and i-k >= 0 and s[i-k] == s[i+k]:
-                if len(best) < 2*k + 1:
-                    best = s[i-k:i+k+1]
+            while i+k < n and i-k >= 0 and string[i-k] == string[i+k]:
+                if f-s < 2*k + 1:
+                    s, f = i-k, i+k+1
                 k += 1
             k = 0
-            while i+1+k < n and i-k >= 0 and s[i-k] == s[i+1+k]:
-                if len(best) < 2*(k+1):
-                    best = s[i-k:i+k+2]
+            while i+1+k < n and i-k >= 0 and string[i-k] == string[i+1+k]:
+                if f-s < 2*(k+1):
+                    s, f = i-k, i+k+2
                 k += 1
-        return best
+        return best[s:f]
