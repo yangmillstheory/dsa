fe05dfdfec35d0437dd42d3bd4596fb0efd5e43e
Victor Alvarez <v.alvarez312@gmail.com>
2018-09-16 12:37:12 -0700

    single pass

--- py/leetcode/two_sum.py
+++ py/leetcode/two_sum.py
@@ -1,11 +1,9 @@
-import collections
-
-
 class Solution(object):
     def twoSum(self, a, q):
         # T(n) = O(n)
         # S(n) = O(n)
-        ind = {x: i for i, x in enumerate(a)}
+        ind = {}
         for i, x in enumerate(a):
             if q-x in ind and ind[q-x] != i:
                 return [i, ind[q-x]]
+            ind[x] = i
