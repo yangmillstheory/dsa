b071cbf9c7a1dbf1f90a0b845b373f7915621ee8
Victor Alvarez <v.alvarez312@gmail.com>
2018-09-16 12:34:54 -0700

    better

--- py/leetcode/two_sum.py
+++ py/leetcode/two_sum.py
@@ -5,14 +5,7 @@ class Solution(object):
     def twoSum(self, a, q):
         # T(n) = O(n)
         # S(n) = O(n)
-        ind = collections.defaultdict(set)
+        ind = {x: i for i, x in enumerate(a)}
         for i, x in enumerate(a):
-            ind[x].add(i)
-        for i, x in enumerate(a):
-            if q-x not in ind:
-                continue
-            j = ind[q-x].pop()
-            if j != i:
-                return [i, j]
-            elif ind[q-x]:
-                return [i, ind[q-x].pop()]
+            if q-x in ind and ind[q-x] != i:
+                return [i, ind[q-x]]
